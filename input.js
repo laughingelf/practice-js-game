741329c11b70f41caf78f2163372e9bb5eb26685
Justin Gery <jaygery90@gmail.com>
2023-04-01 12:42:42 -0500

    initial commit - everything is working as of now

---
+++ input.js
@@ -0,0 +1,6 @@
+export class InputHandler {
+    constructor() {
+        this.keys = [];
+        window.addEventListener('keydown', e => { })
+    }
+}
---
+++ player.js
@@ -0,0 +1,19 @@
+export class Player {
+    constructor(game) {
+        this.game = game;
+        this.width = 100;
+        this.height = 91;
+        this.x = 0;
+        this.y = this.game.height - this.height;
+        this.image = document.getElementById('player');
+    }
+    update(input) {
+        //if (input.includes('ArrowRight')) this.x++;
+        //else if (input.includes('ArrowLeft')) this.x--;
+    }
+    draw(context) {
+        //context.fillStyle = 'red' **this was to make a red rectangle
+        //context.fillRect(this.x, this.y, this.width, this.height);
+        context.drawImage(this.image, 0, 0, this.width, this.height, this.x, this.y, this.width, this.height)
+    }
+}
